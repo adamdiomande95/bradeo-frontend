6f1b8098445208fd31f83014647d8098783ec8f0
Adam Diomande <adamdiomande95@gmail.com>
2026-04-03 22:35:24 +0000

    Connect to production backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://bradeo-backend.onrender.com',
 });
 
 export const getAnnonces = () => API.get('/annonces');
